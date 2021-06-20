0f3e31ca7093e97469677f022ebefdb1137ae639
BigDaddy058 <deekshit.bti@gmail.com>
2021-06-20 11:27:59 +0545

    Made links only channel possible

--- cogs/admin_cmds.py
+++ cogs/admin_cmds.py
@@ -7,10 +7,38 @@ from discord.ext.commands.core import command
 class admin_commands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.badwords = ['fuck', 'motherfucker', 'mugi', 'muji', 'fucking', 'motherfucking', 'dick', 'machikne', 'shit', 'tits',
-                         'bitch', 'whore', 'hoe', 'nigga', 'nigger', 'cunt', 'faggot', 'pussy', 'asshole', 'dickhead', 'bastard',
-                         'wanker', 'randi', 'radi', 'chikne', 'chickney', 'gede', 'lado', 'suck my', 'lick my'
-                         ]
+        self.badwords = [
+            "fuck",
+            "motherfucker",
+            "mugi",
+            "muji",
+            "fucking",
+            "motherfucking",
+            "dick",
+            "machikne",
+            "shit",
+            "tits",
+            "bitch",
+            "whore",
+            "hoe",
+            "nigga",
+            "nigger",
+            "cunt",
+            "faggot",
+            "pussy",
+            "asshole",
+            "dickhead",
+            "bastard",
+            "wanker",
+            "randi",
+            "radi",
+            "chikne",
+            "chickney",
+            "gede",
+            "lado",
+            "suck my",
+            "lick my",
+        ]
 
     @commands.command()
     @commands.has_permissions(kick_members=True)
@@ -20,7 +48,7 @@ class admin_commands(commands.Cog):
 
         else:
             await member.kick(reason=reason)
-            await ctx.send(f'{member.mention}has been kicked')
+            await ctx.send(f"{member.mention}has been kicked")
 
     @kick.error
     async def info_error(self, ctx, error):
@@ -38,21 +66,21 @@ class admin_commands(commands.Cog):
 
         else:
             await member.ban(reason=reason)
-            await ctx.send(f'{member.mention} has been banned')
+            await ctx.send(f"{member.mention} has been banned")
 
     @commands.command()
     @commands.has_permissions(ban_members=True)
     async def unban(self, ctx, *, member):
 
         banned_users = await ctx.guild.bans()
-        member_name, member_discriminator = member.split('#')
+        member_name, member_discriminator = member.split("#")
 
         for banned_entry in banned_users:
             user = banned_entry.user
 
-            if(user.name, user.discriminator) == (member_name, member_discriminator):
+            if (user.name, user.discriminator) == (member_name, member_discriminator):
                 await ctx.guild.unban(user)
-                await ctx.send(f'Unbanned {user.mention}')
+                await ctx.send(f"Unbanned {user.mention}")
                 return
 
     @commands.command()
@@ -64,7 +92,7 @@ class admin_commands(commands.Cog):
             await ctx.send("Slowmode set to 10 sec")
 
         if time == 0:
-            await ctx.send('Slowmode ``Deactivated``!')
+            await ctx.send("Slowmode ``Deactivated``!")
             await ctx.channel.edit(slowmode_delay=0)
         elif time > 21600:
             await ctx.send("You can't set the slowmode above 6 hours!")
@@ -77,13 +105,19 @@ class admin_commands(commands.Cog):
     async def on_message(self, message):
 
         for i in self.badwords:
-            if i.lower() in message.content.lower() or i.upper() in message.content.upper():
+            if (
+                i.lower() in message.content.lower()
+                or i.upper() in message.content.upper()
+            ):
                 if message.author.id == 532960958381817857:
                     return
                 else:
-                    await message.delete()                    
-                    await message.channel.send(f"{message.author.mention} you cannot use that word here", delete_after=5)
-                    self.bot.dispatch('profanity', message,i)
+                    await message.delete()
+                    await message.channel.send(
+                        f"{message.author.mention} you cannot use that word here",
+                        delete_after=5,
+                    )
+                    self.bot.dispatch("profanity", message, i)
                     return
 
     @commands.Cog.listener()
@@ -91,16 +125,20 @@ class admin_commands(commands.Cog):
         guild = message.guild
         if guild.id == 841550614344237057:
             channel = self.bot.get_channel(847342532438655017)
-            embed = discord.Embed(title='**PROFANITY ALERT**',
-                                  description=f"{message.author.name} just said the word ||{word}||", color=discord.Color.blurple()
-                                  )
+            embed = discord.Embed(
+                title="**PROFANITY ALERT**",
+                description=f"{message.author.name} just said the word ||{word}||",
+                color=discord.Color.blurple(),
+            )
             await channel.send(embed=embed)
 
         elif guild.id == 839543471412477952:
             channel = self.bot.get_channel(847375614406950933)
-            embed = discord.Embed(title='**PROFANITY ALERT**',
-                                  description=f"{message.author.name} just said the word ||{word}||", color=discord.Color.blurple()
-                                  )
+            embed = discord.Embed(
+                title="**PROFANITY ALERT**",
+                description=f"{message.author.name} just said the word ||{word}||",
+                color=discord.Color.blurple(),
+            )
             await channel.send(embed=embed)
 
     @commands.Cog.listener()
@@ -118,14 +156,14 @@ class admin_commands(commands.Cog):
             await ctx.channel.send("You cannot mute yourself")
 
         else:
-            var = discord.utils.get(ctx.guild.roles, name='Muted')
+            var = discord.utils.get(ctx.guild.roles, name="Muted")
             await member.add_roles(var)
             await ctx.channel.send(f"{member.mention} has been **MUTED**")
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def unmute(self, ctx, member: discord.Member):
-        role = discord.utils.get(ctx.guild.roles, name='Muted')
+        role = discord.utils.get(ctx.guild.roles, name="Muted")
         await member.remove_roles(role)
         await ctx.channel.send(f"{member.mention} has been **UNMUTED**")
 
@@ -136,11 +174,31 @@ class admin_commands(commands.Cog):
             await ctx.channel.send(f"Please enter a number ", delete_after=5)
 
         elif amount > 100:
-            await ctx.channel.send(f"Please don't enter a number greater than 100", delete_after=5)
+            await ctx.channel.send(
+                f"Please don't enter a number greater than 100", delete_after=5
+            )
 
         else:
-            deleted = await ctx.channel.purge(limit=amount+1)
-            await ctx.channel.send("Deleted {} message(s)".format(len(deleted)), delete_after=8)
+            deleted = await ctx.channel.purge(limit=amount + 1)
+            await ctx.channel.send(
+                "Deleted {} message(s)".format(len(deleted)), delete_after=8
+            )
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        guild = message.guild
+        channel = message.channel
+        if guild.id == 856018962231197726:
+            if channel == self.bot.get_channel(856024909407191050):
+                if "https://" not in message.content:
+                    if message.author.bot:
+                        return
+                    else:
+                        await message.delete()
+                        em = discord.Embed(
+                            description="Only links here", color=discord.Color.red()
+                        )
+                        await channel.send(embed=em, delete_after=5)
 
 
 def setup(bot):
